52e758a7de9f87849bb4c5c6635c6ffd0993f9da
Joseph Constan <jcon5294@gmail.com>
2013-12-21 20:42:45 -0500

    a few small fixes

---
+++ Classes/Core/NSObject+CustomKVC.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+CustomKVC.h
+//  JCModelExample
+//
+//  Created by Joseph Constantakis on 12/21/13.
+//  Copyright (c) 2013 Joseph Constan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (CustomKVC)
+
+@end
---
+++ Classes/Core/NSObject+CustomKVC.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+CustomKVC.m
+//  JCModelExample
+//
+//  Created by Joseph Constantakis on 12/21/13.
+//  Copyright (c) 2013 Joseph Constan. All rights reserved.
+//
+
+#import "NSObject+CustomKVC.h"
+
+@implementation NSObject (CustomKVC)
+
+@end
